81368ba7d5e6bdae0115fe0de5dbada677ea4768
Joe <jwcupit@gmail.com>
2026-04-23 21:51:22 +0100

    Reduce navbar text size and widen it

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,6 +7,8 @@ import MobileNavbarButton from "./components/MobileNavbarButton";
 import NavbarClient from "./NavbarClient";
 import WainroutesLogo from "@/components/Logo/Logo";
 
+import { SearchIcon, BookmarkIcon } from "@/icons/PhosphorIcons";
+
 const navbarId = "navbar";
 
 export default async function Navbar() {
@@ -16,18 +18,23 @@ export default async function Navbar() {
 
       <div className={styles.main}>
         <div className={styles.mainLeft}>
-          <Link href="/" className={`${styles.logo} ${fonts.heading}`}>
-            <WainroutesLogo />
-            Wainroutes
+          <Link href="/" className={styles.logo}>
+            <WainroutesLogo /> Wainroutes
           </Link>
         </div>
 
         <div className={styles.mainRight}>
           <nav id="navbar-nav" className={styles.nav}>
-            <Link href="/walks">Find a walk</Link>
-            <Link href="/about">About</Link>
-            <Link href="/weather">Weather</Link>
+            <Link href="/walks">Walks</Link>
             <Link href="/wainwrights">Wainwrights</Link>
+            <Link href="/weather">Weather</Link>
+            <Link href="/about">About</Link>
+            <Link href="/walks">
+              <SearchIcon /> Search
+            </Link>
+            <Link href="/walks">
+              <BookmarkIcon /> Saved
+            </Link>
           </nav>
 
           <MobileNavbarButton />
