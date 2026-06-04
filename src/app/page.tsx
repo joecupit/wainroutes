9bf48f6229cd841851161ab0137abecafbe1e2cf
Joe <jwcupit@gmail.com>
2026-06-04 22:39:34 +0100

    Update featured walks and recommended orders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,10 +25,10 @@ export function generateMetadata() {
 
 export default function Home() {
   const featuredWalkSlugs = [
+    "the-deepdale-horseshoe",
     "high-rigg-from-thirlmere",
-    "little-and-great-mell-fell",
+    "glenridding-to-threlkeld",
     "the-old-man-of-coniston",
-    "skiddaw-and-little-man",
   ];
   const walks = tempwalks as unknown as Walk[];
   const featuredWalks = featuredWalkSlugs.map((slug) =>
